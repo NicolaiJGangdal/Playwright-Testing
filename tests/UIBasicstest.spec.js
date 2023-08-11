b7094c48ef06b1a5f400f5f2c4dbd8383edcf773
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-11 15:41:25 +0200

    Added more automation
    Updated test to automate a login on a test page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright test", async ({ browser }) => {
+test.only("Browser Context Playwright test", async ({ browser }) => {
   // chrome - plugins/ cookies
   // This method is for when you have inbuilt cookies or preferances you want to be without
   // This is automation:
@@ -8,9 +8,13 @@ test("Browser Context Playwright test", async ({ browser }) => {
   const page = await context.newPage(); //Opens a new page and waits for this to be done before doing next step
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/"); //Navigates the new page to the given url and waits for this to be done before doing next step
   console.log(await page.title());
+  //css, xpath
+  await page.locator("#username").type("rahulshetty"); //Here we are locating the correct input box for username, then we enter the "type" text into it
+  await page.locator("[type='password']").type("learning"); // Same as above but with password
+  await page.locator("#signInBtn").click(); // Same as above but with clicking a button instead of typing, after locating the correct item
 });
 
-test.only("Page Playwright test", async ({ page }) => {
+test("Page Playwright test", async ({ page }) => {
   //This version does the same as the one above, but without opening a cookie free browser
   await page.goto("https://google.com");
   //Get title - asertion
