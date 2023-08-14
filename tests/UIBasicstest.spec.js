22cd165b95612f5a10c32eb95747278cc35f54da
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-14 09:48:37 +0200

    ADD steps to test
    Added more steps to the automation. Now it tries to log in with invalid cridentials first, then with valid. After logging in it retrieves information about the store elements.

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -6,12 +6,23 @@ test.only("Browser Context Playwright test", async ({ browser }) => {
   // This is automation:
   const context = await browser.newContext(); //Opens browser and waits for this to be done before doing next step
   const page = await context.newPage(); //Opens a new page and waits for this to be done before doing next step
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/"); //Navigates the new page to the given url and waits for this to be done before doing next step
   console.log(await page.title());
   //css, xpath
-  await page.locator("#username").type("rahulshetty"); //Here we are locating the correct input box for username, then we enter the "type" text into it
+  await userName.type("rahulshetty"); //Here we are locating the correct input box for username, then we enter the "type" text into it
   await page.locator("[type='password']").type("learning"); // Same as above but with password
-  await page.locator("#signInBtn").click(); // Same as above but with clicking a button instead of typing, after locating the correct item
+  await signIn.click(); // Same as above but with clicking a button instead of typing, after locating the correct item
+  // Wait until this locator shows up on the page
+  console.log(await page.locator("[style*='block']").textContent()); // This waits for content in "error message" then prints it out with console.log
+  await expect(page.locator("[style*='block']")).toContainText('Incorrect'); //Checks if the error message contains a certain word
+  //Now we need to make the test correct the username
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy"); //Fill clears the existing content and writes in new userName
+  await signIn.click(); //attempting to log in again
+  console.log(await page.locator(".card-body a").first().textContent()); //Searching for a card-element on the page after successfully logging in, searching for the first element in the array - then prints it
+  console.log(await page.locator(".card-body a").nth(1).textContent());
 });
 
 test("Page Playwright test", async ({ page }) => {
