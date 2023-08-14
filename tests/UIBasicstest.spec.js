04cba90e189453b5fe048a50d83dade2f37992a7
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-14 12:59:07 +0200

    ADD new test file
    Added new test file, and made different examples of how to automate logiin's on different pages with different htmls

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,35 +1,30 @@
 const { test, expect } = require("@playwright/test");
 
-test.only("Browser Context Playwright test", async ({ browser }) => {
-  // chrome - plugins/ cookies
-  // This method is for when you have inbuilt cookies or preferances you want to be without
-  // This is automation:
-  const context = await browser.newContext(); //Opens browser and waits for this to be done before doing next step
-  const page = await context.newPage(); //Opens a new page and waits for this to be done before doing next step
-  const userName = page.locator("#username");
-  const signIn = page.locator("#signInBtn");
-  await page.goto("https://rahulshettyacademy.com/loginpagePractise/"); //Navigates the new page to the given url and waits for this to be done before doing next step
-  console.log(await page.title());
-  //css, xpath
-  await userName.type("rahulshetty"); //Here we are locating the correct input box for username, then we enter the "type" text into it
-  await page.locator("[type='password']").type("learning"); // Same as above but with password
-  await signIn.click(); // Same as above but with clicking a button instead of typing, after locating the correct item
-  // Wait until this locator shows up on the page
-  console.log(await page.locator("[style*='block']").textContent()); // This waits for content in "error message" then prints it out with console.log
-  await expect(page.locator("[style*='block']")).toContainText('Incorrect'); //Checks if the error message contains a certain word
-  //Now we need to make the test correct the username
-  await userName.fill("");
-  await userName.fill("rahulshettyacademy"); //Fill clears the existing content and writes in new userName
-  await signIn.click(); //attempting to log in again
-  console.log(await page.locator(".card-body a").first().textContent()); //Searching for a card-element on the page after successfully logging in, searching for the first element in the array - then prints it
-  console.log(await page.locator(".card-body a").nth(1).textContent());
-});
 
-test("Page Playwright test", async ({ page }) => {
-  //This version does the same as the one above, but without opening a cookie free browser
-  await page.goto("https://google.com");
-  //Get title - asertion
-  console.log(await page.title());
-  await expect(page).toHaveTitle("Google");
-  console.log("Branch Test");
+
+// Create new test to access this site "https://rahulshettyacademy.com/client/"
+// Log In - Access First Item On Page
+// Username: nfotball_96@hotmail.com PW: Playwright123
+
+test("Solo Browser Context Test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  const cardTitles = page.locator(".card-body b");
+
+  await page.goto("https://rahulshettyacademy.com/client/");
+  await page.locator("#userEmail").type("nfotball_96@hotmail.com");
+  await page.locator("[type='password']").type("Playwright123");
+  await page.locator("#login").click();
+
+  //  console.log(await page.locator("[style*='block']").textContent());
+  //  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+  //  console.log(await cardTitles.first().textContent());
+  
+  //  await page.waitForLoadState('networkidle');
+
+  await cardTitles.first().waitFor();
+  const titles = await cardTitles.allTextContents();
+  console.log(titles);
 });
---
+++ tests/UIOldTest.spec.js
@@ -0,0 +1,38 @@
+const { test, expect } = require("@playwright/test");
+
+test("Browser Context Playwright test", async ({ browser }) => {
+  // chrome - plugins/ cookies
+  // This method is for when you have inbuilt cookies or preferances you want to be without
+  // This is automation:
+  const context = await browser.newContext(); //Opens browser and waits for this to be done before doing next step
+  const page = await context.newPage(); //Opens a new page and waits for this to be done before doing next step
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
+  const cardTitles = page.locator(".card-body a");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/"); //Navigates the new page to the given url and waits for this to be done before doing next step
+  // console.log(await page.title());
+  //css, xpath
+  await userName.type("rahulshetty"); //Here we are locating the correct input box for username, then we enter the "type" text into it
+  await page.locator("[type='password']").type("learning"); // Same as above but with password
+  await signIn.click(); // Same as above but with clicking a button instead of typing, after locating the correct item
+  // Wait until this locator shows up on the page
+  console.log(await page.locator("[style*='block']").textContent()); // This waits for content in "error message" then prints it out with console.log
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect"); //Checks if the error message contains a certain word
+  //Now we need to make the test correct the username
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy"); //Fill clears the existing content and writes in new userName
+  await signIn.click(); //attempting to log in again
+  console.log(await cardTitles.first().textContent()); //Searching for a card-element on the page after successfully logging in, searching for the first element in the array - then prints it
+  // console.log(await cardTitles.nth(1).textContent()); // same as above, but retrieves the second element in the array
+  const allTitles = await cardTitles.allTextContents(); // Above we grabbed the first and second item in a list. Now we are grabbing all
+  console.log(allTitles);
+});
+
+test("Page Playwright test", async ({ page }) => {
+  //This version does the same as the one above, but without opening a cookie free browser
+  await page.goto("https://google.com");
+  //Get title - asertion
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
+  console.log("Branch Test");
+});
